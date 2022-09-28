31a6589e2a1067bda28ee67cce4a7141a01cd406
michal <galeriakoloru90210@gmail.com>
2022-09-29 01:31:18 +0200

    add dark theme/light finish css for header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,28 +1,46 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useTheme } from 'next-themes';
 
 // external imports
 import images from '../assets/app'
 
 
 const Header = () => {
+  const { theme, setTheme } = useTheme();
+
   return (
-    <header className="sticky top-0 z-30 flex w-full items-center justify-between p-4 bg-[#E7ECEE] dark:bg-[#0F0F12]">
-        <div className="flex items-center justify-center md:w-1/5">
-            <Link href="/">
-            <div className="relative h-[90px] w-[90px] cursor-pointer opacity-80 transition hover:opacity-100">
-                <Image src={images.logo} layout="fill" objectFit="contain" alt="logo"/>
-            </div>
-            </Link>
-            <div>
-                <Link className="headerLink" href="/">Produkty</Link>
-                <Link className="headerLink" href="/art">Art</Link>
-                <Link className="headerLink" href="/sale">Promocje</Link>
-                <Link className="headerLink" href="/free-graffiti">Free Graffiti</Link>
-            </div>
-        </div>
-    </header>
+    <header className="sticky top-0 z-30 flex w-full items-center justify-between p-4 dark:bg-[#0f0f12] bg-[#E7ECEE]">
+    <div className="flex items-center justify-center md:w-1/5">
+      <Link href="/">
+      <div className="relative h-[90px] w-[90px] cursor-pointer opacity-80 transition hover:opacity-100">
+        <Image src={images.logo} className={theme === 'light' ? 'filter invert' : ''} layout="fill" objectFit="contain" alt="logo"/>
+      </div>
+    </Link>
+    </div>
+    <div className="hidden flex-1 items-center justify-center space-x-8 md:flex">
+      <a className="headerLink">Produkty</a>
+      <a className="headerLink">Nowości</a>
+      <a className="headerLink">Wkrótce</a>
+      <a className="headerLink">Free Graffiti</a>
+    </div>
+    <div className="flex items-center justify-center gap-x-4 md:w-1/5">
+      <div className="flex items-center ml-3">
+        <input
+          type="checkbox"
+          className="checkbox"
+          id="checkbox"
+          onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        />
+        <label htmlFor="checkbox" className="flexBetween w-8 h-4 bg-black rounded-2xl p-1 relative label">
+          <i className="fas fa-sun" />
+          <i className="fas fa-moon" />
+          <div className="w-3 h-3 absolute bg-white rounded-full ball" />
+        </label>
+      </div>
+    </div>    
+  </header>
   )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { ThemeProvider } from 'next-themes'
+import Script from 'next/script'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+  return (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-gk-dark bg-white min-h-screen">
+      <Component {...pageProps} />
+    </div>
+    <Script src="https://kit.fontawesome.com/99c5f6502c.js" crossOrigin="anonymous" />
+  </ThemeProvider>
+  )
 }
 
 export default MyApp
