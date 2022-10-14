4f84a453de7a76220408e93c15eccfa488ef6d74
Michał Bryk <galeriakoloru90210@gmail.com>
2022-10-15 00:07:48 +0200

    add routes do single product

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,14 +1,15 @@
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import React from 'react'
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
 
 import { urlFor } from '../sanity';
+import Link from 'next/link';
 
 
 
 const Product = ({product}) => {
   const router = useRouter()
-
   return (
     <div className="flex h-fit w-[320px] select-none flex-col space-y-3 rounded-xl bg-[#e8e8e8] dark:bg-[#35383C] p-8 md:h-[500px] md:w-[400px] md:p-10">
       <div className="relative h-64 w-full md:h-72">
@@ -19,11 +20,13 @@ const Product = ({product}) => {
           <p>{product.title}</p>
           {router.pathname === `/free-graffiti` ? "" : <p>{product.price} PLN</p>}
         </div>
-        <div 
-        className="flex h-16 w-16 flex-shrink-0 cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-violet-500 to-blue-500"
-        >
-
-        </div>
+        <Link href={`${router.pathname}/${product.slug.current}`}>
+          <div 
+          className="flex h-16 w-16 flex-shrink-0 cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-violet-500 to-blue-500"
+          >
+          <MagnifyingGlassIcon className="h-6 w-6 text-white" />
+          </div>
+        </Link> 
       </div>
     </div>
   )
