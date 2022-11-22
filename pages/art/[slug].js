10a53476a69a7854dbbb55d2b90b8001d3d81122
Michał Bryk <galeriakoloru90210@gmail.com>
2022-11-22 23:31:59 +0100

    change h of art detail page

--- pages/art/[slug].js
+++ pages/art/[slug].js
@@ -14,7 +14,7 @@ const ProductDetails = ({product}) => {
   
 
   return (
-    <div className="w-screen nav-h overflow-hidden bg-[#E7ECEE] dark:bg-[#0f0f12] pt-8 flex">
+    <div className="w-screen h-screen nav-h overflow-hidden bg-[#E7ECEE] dark:bg-[#0f0f12] pt-8 flex">
       <div className="flex gap-[40px] p-[40px] pt-[60px]">
         <div className="flex-col">
           <div className="relative flex w-[450px] h-[450px] dark:bg-[#24252D] bg-[#f4f4f4] rounded-xl">
