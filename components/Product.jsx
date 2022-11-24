f62624745df0cb4a3836624b5838e8bb866eaabb
Michał Bryk <galeriakoloru90210@gmail.com>
2022-11-24 02:01:31 +0100

    css adjustments for single sale product for tablet

--- components/Product.jsx
+++ components/Product.jsx
@@ -11,18 +11,18 @@ import Link from 'next/link';
 const Product = ({product}) => {
   const router = useRouter()
   return (
-    <div className="flex h-fit w-[320px] select-none flex-col space-y-3 rounded-xl bg-[#e8e8e8] dark:bg-[#35383C] p-8 md:h-[500px] md:w-[400px] md:p-10">
-      <div className="relative h-64 w-full md:h-72">
+    <div className="flex h-fit w-[320px] select-none flex-col space-y-3 rounded-xl bg-[#e8e8e8] dark:bg-[#35383C] p-8 xl:h-[500px] xl:w-[400px] md:p-10">
+      <div className="relative h-64 w-full xl:h-72">
         <Image src={urlFor(product.image[0]).url()} layout="fill" objectFit="contain"/>
       </div>
       <div className="flex flex-1 items-center justify-between space-x-3">
-        <div className="space-y-2 text-xl text-black/80 dark:text-white md:text-2xl">
+        <div className="space-y-2 text-lg xl:text-2xl text-black/80 dark:text-white">
           <p>{product.title}</p>
           {router.pathname === `/free-graffiti` ? "" : <p>{product.salePrice ? product.salePrice : product.price} PLN</p>}
         </div>
         <Link href={router.pathname === '/' ? `${product.slug.current}` : `${router.pathname}/${product.slug.current}`}>
           <div 
-          className="flex h-16 w-16 flex-shrink-0 cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-violet-500 to-blue-500"
+          className="flex h-12 xl:h-16 w-12 xl:w-16 flex-shrink-0 cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-violet-500 to-blue-500"
           >
           <MagnifyingGlassIcon className="h-6 w-6 text-white" />
           </div>
--- pages/sale/[slug].js
+++ pages/sale/[slug].js
@@ -13,15 +13,15 @@ const ProductDetails = ({product}) => {
   const [index, setIndex] = useState(0);
 
   return (
-    <div className="w-screen nav-h overflow-hidden bg-[#E7ECEE] dark:bg-[#0f0f12] pt-8 flex">
-      <div className="flex gap-[40px] p-[40px] pt-[60px]">
+    <div className="w-screen h-screen nav-h overflow-hidden bg-[#E7ECEE] dark:bg-[#0f0f12] pt-8 flex">
+      <div className="flex gap-[10px] xl:gap-[40px] p-[10px] xl:p-[40px] pt-[10px] xl:pt-[60px] pl-8">
         <div className="flex-col">
-          <div className="relative flex w-[450px] h-[450px] dark:bg-[#24252D] bg-[#f4f4f4] rounded-xl">
+          <div className="relative flex xl:w-[450px] xl:h-[450px] w-[250px] h-[250px] dark:bg-[#24252D] bg-[#f4f4f4] rounded-xl">
             <Image src={urlFor(product?.image && product?.image[index]).url()} layout="fill" objectFit="contain"/>
           </div>
-            <div className="flex gap-4 pt-6">
+            <div className="flex gap-2 xl:gap-4 pt-4 xl:pt-6">
               {product.image?.map((item,i) => (
-                <div key={i} className="relative w-[100px] h-[100px]">
+                <div key={i} className="relative w-[62px] h-[62px] xl:w-[100px] xl:h-[100px]">
                 <Image
                  key={i}
                  src={urlFor(item).url()}
@@ -37,18 +37,18 @@ const ProductDetails = ({product}) => {
             <div className="flex-col justify-center items-center pt-2">
               {product.additionalInfo ? (
               <>
-                <h1 className="tracking-wide text-2xl pt-4">Dodatkowe informacje:</h1>
+                <h1 className="tracking-wide text-xl xl:text-2xl pt-2 xl:pt-4">Dodatkowe informacje:</h1>
                 <div className="flex items-center">
                   {product.additionalInfo.map((item) => (
-                    <div key={item._key} className="dark:border-white/10 border-black/10 border-2 p-7 flex items-center justify-center mr-2 mt-3 uppercase w-[120px] rounded-md h-[40px] text-center">{item.text}</div>
+                    <div key={item._key} className="dark:border-white/10 border-black/10 border-2 inline-flex p-6 xl:p-7 items-center justify-center mr-2 mt-3 uppercase w-[90px] xl:w-[120px] h-[25px] xl:h-[30px] text-xs xl:text-base text-center rounded-md">{item.text}</div>
                   ))}
               </div>
               </> 
               ) : (
                 <>
-                <h1 className="tracking-wide text-2xl pt-4">Dostępne rozmiary:</h1>
+                <h1 className="tracking-wide text-xl xl:text-2xl pt-2 xl:pt-4">Dostępne rozmiary:</h1>
                 {product.rozmiar && Object.entries(product?.rozmiar).map((size, i ) => (
-                <span key={i} className={`${size[1] == false ? 'line-through text-red-500' : '' } dark:border-white/10 border-black/10 border-2 inline-flex p-6 justify-center items-center mr-2 mt-3 uppercase w-[20px] h-[20px] text-center rounded-md`}>{size}</span>
+                <span key={i} className={`${size[1] == false ? 'line-through text-red-500' : '' } dark:border-white/10 border-black/10 border-2 inline-flex p-4 xl:p-6 justify-center items-center mr-2 mt-3 uppercase w-[10px] xl:w-[20px] h-[10px] xl:h-[20px] text-sm xl:text-base text-center rounded-md`}>{size}</span>
               ))}
                 </>
               )}
@@ -56,29 +56,29 @@ const ProductDetails = ({product}) => {
             </div>
         </div>  
       </div>
-      <div className="w-[400px] h-[400px] flex-col flex-1 justify-center items-start pt-[60px]">
+      <div className="w-[250px] xl:w-[350px] h-[250px] xl:h-[350px] flex-col flex-1 justify-center items-start pt-[10px] xl:pt-[60px]">
         <div className="flex flex-col justify-center items-center">
-          <h1 className="text-5xl">{product.title}</h1>
-            <hr  className="w-[90%] mt-3 text-[0.2] bg-black"/>
-            <p className="w-[70%] h-3/5 pt-[50px] text-xl">{product.description}</p>
+          <h1 className="text-3xl xl:text-5xl">{product.title}</h1>
+            <hr  className="w-3/4 xl:w-4/5 mt-3 text-[0.2] bg-black"/>
+            <p className="w-[70%] h-3/5 pt-[20px] xl:pt-[40px] text-base xl:text-xl">{product.description}</p>
         </div>
-        <div className="pt-[10px] tracking-wide flex-col">
+        <div className="pt-[10px] xl:pt-[30px] tracking-wide flex-col">
                 {product?.colors ? (
-                <div className="px-[10px] w-5/6 mx-auto">
-                <h2 className="flex justify-center items-center text-xl pb-2">Kolory:</h2>
-                <div className="flex flex-wrap justify-center items-center gap-8">
+                <div className="px-[10px] w-2/3 xl:w-5/6 mx-auto">
+                <h2 className="flex justify-center items-center text-base xl:text-xl pb-2">Kolory:</h2>
+                <div className="flex flex-wrap justify-center items-center gap-5 xl:gap-8">
                   {product.colors.map((item,) => (
-                    <div key={item._key} style={{ backgroundColor: `${item.color}` }} className={`w-[100px] h-[35px] rounded-md`}></div>
+                    <div key={item._key} style={{ backgroundColor: `${item.color}` }} className={`w-[45px] h-[20px] xl:w-[100px] xl:h-[35px] rounded-md`}></div>
                   ))}
                 </div>
                 </div>
                 ) : (
-                <div className="flex-col justify-center pt-5">
-                <h2 className="flex justify-center items-center text-xl">Tabela rozmiarów:</h2>
-                <div className="flex justify-center items-center w-full pt-6">
-                <table className="bg-white dark:bg-[#0f0f12] text-black dark:text-white table-fixed border-collapse w-[70%] shadow-none rounded-lg text-lg">
+                <div className="pt-[10px] xl:pt-[30px] tracking-wide flex-col">
+                <h2 className="flex justify-center items-center text-base xl:text-xl">Tabela rozmiarów:</h2>
+                <div className="flex justify-center items-center w-full pt-2 xl:pt-6">
+                <table className="bg-white dark:bg-[#0f0f12] text-black dark:text-white table-fixed border-collapse w-[70%] shadow-none rounded-lg text-sm xl:text-lg">
                   <tbody> 
-                    <tr className="text-center h-[40px]">
+                    <tr className="text-center h-[20px] xl:h-[40px]">
                     <td>Rozmiar</td>
                     <td>S</td> 
                     <td>M</td> 
@@ -86,13 +86,13 @@ const ProductDetails = ({product}) => {
                     <td>XL</td> 
                     <td>XXL</td> 
                   </tr>  
-                  <tr key="width" className="text-center h-[40px]">
+                  <tr key="width" className="text-center h-[20px] xl:h-[40px]">
                     <td>Szerokość</td>
                     {product.rozmiarowka.map((item) => (
                       <td key={item.key}>{item.width}</td>
                     ))}
                   </tr> 
-                  <tr className="text-center h-[40px]">
+                  <tr className="text-center h-[20px] xl:h-[40px]">
                     <td>Długość</td>
                     {product.rozmiarowka.map((item) => (
                       <td key={item.key}>{item.height}</td>
@@ -104,14 +104,14 @@ const ProductDetails = ({product}) => {
                 </div>
                 )            
                 } 
-                <div className="flex w-3/5 mx-auto justify-between items-center pt-10 uppercase text-2xl border-b border-b-black/40 dark:border-b-white/50 pb-1">
+                <div className="flex w-3/6 xl:w-3/5 mx-auto justify-between items-center pt-5 xl:pt-10 uppercase text-base xl:text-2xl border-b border-b-black/40 dark:border-b-white/50 pb-1">
                   <p>cena:</p>
                   <div className="flex">
                   <p className="text-red-500 line-through px-2">{product.price}</p>
                   <p>{product.salePrice} pln</p>
                   </div>
                 </div>
-                <div className="flex justify-center pt-[50px]">
+                <div className="flex justify-center pt-[50px] xl:pt-[120px]">
                   <Button title="Powrót do strony głównej" onClick={() => router.back()}/>
                 </div>  
               </div>
